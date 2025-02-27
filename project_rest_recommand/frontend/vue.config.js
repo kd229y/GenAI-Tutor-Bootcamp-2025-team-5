3fb87dd014a39b854f3b39ab8873fc33859c27da
Kai-Tung <ktyang@cs.nycu.edu.tw>
2025-02-27 20:56:23 +0800

    discard the configure of publicPath

--- project_rest_recommand/frontend/vue.config.js
+++ project_rest_recommand/frontend/vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-  ?'GenAI-Tutor-Bootcamp-2025-team-5'
-  :'/'
 })
