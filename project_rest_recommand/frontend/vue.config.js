aa54cd07c7d56edadc6dd49bdd13d2f4d1bc24ed
Kai-Tung <ktyang@cs.nycu.edu.tw>
2025-03-05 13:20:33 +0800

    configure vue.config.js

--- project_rest_recommand/frontend/vue.config.js
+++ project_rest_recommand/frontend/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  filenameHashing: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/GenAI-Tutor-Bootcamp-2025-team-5/' : '/'
 })
